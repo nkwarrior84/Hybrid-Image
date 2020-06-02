bcfae9516ea2600db4fa2c2ea61566c961228259
nkwarrior84 <32726683+nkwarrior84@users.noreply.github.com>
2020-06-02 11:59:08 +0530

    Code With inbuilt functions

---
+++ With Inbuilt functions/hybrid_image_with_inbuilt.py
@@ -0,0 +1,54 @@
+import numpy
+from numpy.fft import fft2, ifft2, fftshift, ifftshift
+from scipy import misc
+from scipy import ndimage
+import math
+
+def scaleSpectrum(A):
+   return numpy.real(numpy.log10(numpy.absolute(A) + numpy.ones(A.shape)))
+
+
+# sample values from a spherical gaussian function from the center of the image
+def makeGaussianFilter(numRows, numCols, sigma, highPass=True):
+   centerI = int(numRows/2) + 1 if numRows % 2 == 1 else int(numRows/2)
+   centerJ = int(numCols/2) + 1 if numCols % 2 == 1 else int(numCols/2)
+
+   def gaussian(i,j):
+      coefficient = math.exp(-1.0 * ((i - centerI)**2 + (j - centerJ)**2) / (2 * sigma**2))
+      return 1 - coefficient if highPass else coefficient
+
+   return numpy.array([[gaussian(i,j) for j in range(numCols)] for i in range(numRows)])
+
+
+def filterDFT(imageMatrix, filterMatrix):
+   shiftedDFT = fftshift(fft2(imageMatrix))
+   misc.imsave("dft.png", scaleSpectrum(shiftedDFT))
+
+   filteredDFT = shiftedDFT * filterMatrix
+   misc.imsave("filtered-dft.png", scaleSpectrum(filteredDFT))
+   return ifft2(ifftshift(filteredDFT))
+
+
+def lowPass(imageMatrix, sigma):
+   n,m = imageMatrix.shape
+   return filterDFT(imageMatrix, makeGaussianFilter(n, m, sigma, highPass=False))
+
+
+def highPass(imageMatrix, sigma):
+   n,m = imageMatrix.shape
+   return filterDFT(imageMatrix, makeGaussianFilter(n, m, sigma, highPass=True))
+
+
+def hybridImage(highFreqImg, lowFreqImg, sigmaHigh, sigmaLow):
+   highPassed = highPass(highFreqImg, sigmaHigh)
+   lowPassed = lowPass(lowFreqImg, sigmaLow)
+
+   return highPassed + lowPassed
+
+
+if __name__ == "__main__":
+   einstein = ndimage.imread("einstein.png", flatten=True)
+   marilyn = ndimage.imread("marilyn.png", flatten=True)
+
+   hybrid = hybridImage(einstein, marilyn, 25, 10)
+   misc.imsave("marilyn-einstein.png", numpy.real(hybrid))
